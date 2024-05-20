8263b532fd228ffe0fa8205311b235843e4116ee
Milan Thummar <thummar.fb@gmail.com>
2024-05-20 19:59:36 +0200

    update the webpack with plugins for Minify JS and CSS for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,68 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/Slider.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "slider.js",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/slider.ts",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "slider.js",
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer()],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/slider.html",
+      }),
+      ...(isProduction
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "slider.css",
+            }),
+          ]
+        : []),
     ],
-  },
+    optimization: {
+      minimize: isProduction,
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              parallel: true,
+            }),
+          ]
+        : [],
+    },
+  };
 };
